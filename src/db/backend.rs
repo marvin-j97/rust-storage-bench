4fd251be934f9bed0818dd35f168378475e9a7cc
marvin-j97 <marvin.janke.97@gmail.com>
2025-02-19 22:29:44 +0100

    update redb

--- src/db/backend.rs
+++ src/db/backend.rs
@@ -52,7 +52,7 @@ impl std::fmt::Display for Backend {
                 #[cfg(feature = "localfjall")]
                 Self::LocalFjall => "fjall nightly",
 
-                Self::Redb => "redb 2.3.0",
+                Self::Redb => "redb 2.4.0",
 
                 #[cfg(feature = "heed")]
                 Self::Heed => "heed 0.20.5",
