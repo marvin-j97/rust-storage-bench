f3bf23a8f8b58ca0afbbc8326733e35f62b0d98f
marvin-j97 <marvin.janke.97@gmail.com>
2025-05-05 14:51:31 +0200

    change colours

--- report/src/util.ts
+++ report/src/util.ts
@@ -14,55 +14,61 @@ export function formatNano(nanos: number): string {
 	return `${(nanos / 1_000 / 1_000).toFixed(1)}ms`;
 }
 
-export function chooseColor(backend: string): string {
-	if (backend.includes("redb")) {
+const fallbackColors = [
+	"#ffffff",
+	"#ee5555",
+	"#4455FF",
+	"#38bdf8",
+	"#fbbf24",
+	"#ff7700",
+	"#00ffdd",
+	"#f472b6",
+	"#99aabb",
+];
+
+export function chooseColor(backendName: string): string {
+	if (backendName.includes("redb")) {
 		return "#ffffff";
 	}
-	if (backend.includes("sled")) {
+	if (backendName.includes("sled")) {
 		return "#ee5555";
 	}
-	if (backend.includes("localfjall")) {
+	if (backendName.includes("local")) {
 		return "#4455FF";
 	}
-	if (backend.includes("fjall")) {
+	if (backendName.includes("fjall")) {
 		return "#38bdf8";
 	}
-	if (backend.includes("rocksdb")) {
+	if (backendName.includes("rocksdb")) {
 		return "#fbbf24";
 	}
-	if (backend.includes("heed")) {
+	if (backendName.includes("heed")) {
 		return "#ff7700";
 	}
-	if (backend.includes("sqlite")) {
+	if (backendName.includes("sqlite")) {
 		return "#00ffdd";
 	}
-	if (backend.includes("canopy")) {
+	if (backendName.includes("canopy")) {
 		return "#f472b6";
 	}
-	return "#99aabb"
+	if (backendName.includes("leveldb")) {
+		return "#4499ff";
+	}
+	const code = [...backendName]
+		.reduce((acc, c) => acc + c.charCodeAt(0), backendName.length);
+
+	return fallbackColors[code % fallbackColors.length];
 }
 
 export function isLsm(backend: string): boolean {
-	if (backend.includes("redb")) {
-		return false;
-	}
-	if (backend.includes("sled")) {
-		return false;
-	}
-	if (backend.includes("localfjall")) {
-		return true;
-	}
 	if (backend.includes("fjall")) {
 		return true;
 	}
 	if (backend.includes("rocksdb")) {
 		return true;
 	}
-	if (backend.includes("heed")) {
-		return false;
-	}
-	if (backend.includes("sqlite")) {
-		return false;
+	if (backend.includes("leveldb")) {
+		return true;
 	}
 	return false;
 }
