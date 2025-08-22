65baf01360be139b309f55f27a93aa3ddf6df194
marvin-j97 <marvin.janke.97@gmail.com>
2025-08-22 16:06:07 +0200

    fix

--- src/args.rs
+++ src/args.rs
@@ -1,4 +1,4 @@
-use crate::workload::feed::Options as FeedOptions;
+use crate::workload::{event_log::Options as EventLogOptions, feed::Options as FeedOptions};
 use crate::workload::{queue::Options as QueueOptions, ycsb::Options as YcsbOptions};
 use crate::{corpus::Corpus, db::Backend};
 use clap::{Args as ClapArgs, Parser, Subcommand, ValueEnum};
