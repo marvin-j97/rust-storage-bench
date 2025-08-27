8c0166dca1c3e3c027bbe134a60a9ac66b6d2cbd
marvin-j97 <marvin.janke.97@gmail.com>
2025-08-27 13:43:48 +0200

    disable oom killer temp

--- src/monitor.rs
+++ src/monitor.rs
@@ -67,10 +67,10 @@ pub fn start_monitor(
                 let cpu = child.cpu_usage();
                 let mem_kib = (child.memory() as f64 / 1_024.0) as u64;
 
-                if mem_kib >= 16 * 1_024 * 1_024 {
-                    log::error!("OOM KILLER!! Exceeded 16GB of memory");
-                    std::process::exit(666);
-                }
+                // if mem_kib >= 16 * 1_024 * 1_024 {
+                //     log::error!("OOM KILLER!! Exceeded 16GB of memory");
+                //     std::process::exit(666);
+                // }
 
                 let disk_space_kib = fs_extra::dir::get_size(&data_dir).unwrap_or_default() / 1_024;
 
